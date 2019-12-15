88da7d12eacf799ec3931d53d7734800f11c69a4
JohnRamboJedi <58915584+JohnRamboJedi@users.noreply.github.com>
2019-12-15 13:56:07 -0700

    added print

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-def trade_action (current_stock, purchase_price, investment):
+def trade_action (current_stock, purchase_price, current_price, investment):
 	import math
 	current_shares = int(current_stock)
 	purchase_price = float(purchase_price)
-	market_price = float(current_prcie)
+	market_price = float(current_price)
 	available_funds = float(investment)
 	transaction_fee = 10
 	actual_availale_funds = available_funds - transaction_fee
 	amount_of_shares_to_buy = actual_availale_funds / market_price
-	shares_to_buy_rounded - math.floor(amount_of_shares_to_buy)
+	shares_to_buy_rounded = math.floor(amount_of_shares_to_buy)
 	profitable_buy = (purchase_price - market_price) * shares_to_buy_rounded
 	proftiable_sell = (market_price - purchase_price) * current_shares
 
@@ -21,3 +21,6 @@ def trade_action (current_stock, purchase_price, investment):
 		return "Hold shares"
 	elif actual_availale_funds >= market_price:
 		return "Buy " + str(shares_to_buy_rounded) + " shares"
+
+trade_action (10, 25, 15, 100)
+print (trade_action (10, 25, 15, 100))
